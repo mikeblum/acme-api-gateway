fb371cbe24bc959877e899646dc21af19e85bbf2
mblum <me@michaelblum.me>
2016-07-26 16:47:06 -0500

    S3 implementation of creating a .well-known/acme-challenge/_token_

--- certs/get_certs.py
+++ certs/get_certs.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import pprint
 
 import boto3
 from boto3.s3.transfer import S3Transfer
@@ -12,9 +13,13 @@ from acme import client
 from acme import messages
 from acme import jose
 
+from acme import challenges
+from certbot import achallenges
+
 from Crypto.PublicKey import RSA
 
 logging.basicConfig(level=logging.DEBUG)
+pp = pprint.PrettyPrinter(indent=4)
 
 
 DIRECTORY_URL = 'https://acme-staging.api.letsencrypt.org/directory'
@@ -22,16 +27,21 @@ BITS = 2048  # minimum for Boulder
 DOMAIN = 'api.magicka.io'
 S3_BUCKET = DOMAIN
 
-# upload ACME challenge to S3 @ .well-known/acme-challenge
-s3_client = boto3.client('s3', 'us-east-1')
-s3_transfer = S3Transfer(s3_client)
-with open('letsencrypt.txt', 'w') as lets_encrypt:
-    lets_encrypt.write("letsencrypt")
+def upload_token(challenge):
+    # upload ACME challenge to S3 @ .well-known/acme-challenge
+    s3_client = boto3.client('s3', 'us-east-1')
+    s3_transfer = S3Transfer(s3_client)
 
-s3_transfer.upload_file('letsencrypt.txt', S3_BUCKET, 
-                      '.well-known/acme-challenge/letsencrypt',
-                       extra_args={'ContentType': "text/plain"}
-                    )
+    token = challenge['token']
+    logging.info('Uploading challenge token to S3: {}'.format(token))
+    s3_path = '.well-known/acme-challenge/{}'.format(token)
+    with open('letsencrypt.txt', 'w') as lets_encrypt:
+        lets_encrypt.write(token)
+
+    s3_transfer.upload_file('letsencrypt.txt', S3_BUCKET, 
+                          s3_path,
+                           extra_args={'ContentType': "text/plain"}
+                        )
 
 # generate_private_key requires cryptography>=0.5
 key = jose.JWKRSA(key=rsa.generate_private_key(
@@ -40,17 +50,24 @@ key = jose.JWKRSA(key=rsa.generate_private_key(
     backend=default_backend()))
 acme = client.Client(DIRECTORY_URL, key)
 
-regr = acme.register()
+new_reg = messages.NewRegistration.from_data(email='admin@magicka.io')
+regr = acme.register(new_reg)
 logging.info('Auto-accepting TOS: %s', regr.terms_of_service)
 acme.agree_to_tos(regr)
 logging.debug(regr)
 
-authzr = acme.request_challenges(
-    identifier=messages.Identifier(typ=messages.IDENTIFIER_FQDN, value=DOMAIN),
-    new_authzr_uri=regr.new_authzr_uri)
-logging.debug(authzr)
+authzr = acme.request_domain_challenges(
+    DOMAIN,
+    new_authzr_uri=regr.new_authzr_uri
+)
+
+for challenge in authzr.body.challenges:
+    challenge = challenge.to_json()
+    if challenge['type'] == 'dns-01':
+        upload_token(challenge)
 
 authzr, authzr_response = acme.poll(authzr)
+logging.debug(authzr_response)
 
 fo = open('csr.der', 'r')
 csrreq = OpenSSL.crypto.load_certificate_request(OpenSSL.crypto.FILETYPE_PEM, fo.read())
