1317c37a543c24dfa01204343a0f4bc0c0a5749f
mblum <me@mblum.me>
2016-10-11 22:32:22 -0500

    can now renew a LE cert and upload to IAM -> API Gateway

--- env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export AWS_IAM_USER=ipbot
-export AWS_ACCESS_KEY=AKIAJLS6RMYEDX6COGYQ
-export AWS_SECRET_ACCESS_KEY=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
-export AWS_REGION=us-east-1
--- hooks/hook.py
+++ hooks/hook.py
@@ -4,12 +4,14 @@ import logging
 import os
 from pprint import pprint
 import sys
+import time
 from time import sleep
 
 import tldextract
 import dns.resolver
 
 import boto3
+import botocore
 import route53
 
 # logging.basicConfig(level=logging.DEBUG)
@@ -17,12 +19,9 @@ TTL = 60 # seconds
 CERT_FILE = "cert.pem"
 PRIVATE_KEY_FILE = "privkey.pem"
 CHAIN_FILE = "chain.pem"
-FULL_CHAIN_FILE = "fullchain.pem"
+FULL_CHAIN_FILE = "full_chain.pem"
 
-def upload_dns_challange(domain, challenge):
-    """
-    Create TXT record for the specified domain in Route53
-    """
+def get_route53_zone(domain):
     route53_conn = route53.connect(
         aws_access_key_id=os.environ['AWS_ACCESS_KEY'],
         aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
@@ -37,30 +36,33 @@ def upload_dns_challange(domain, challenge):
             route53_id = zone.id
     if route53_id is None:
         raise ValueError('domain {} not found in Route53 hosted zones'.format(tld))
-    le_challange = challenge
-    print('setting dns-challange for letsencrypt key: {0}'.format(le_challange))
-    record_name = '_acme-challenge.{0}.'.format(domain) 
     # create txt record for the specified domain
-    zone = route53_conn.get_hosted_zone_by_id(route53_id)
-    # check for existing TXT challange record
-    txt_record = None
-    for record_set in zone.record_sets:
-        if record_set.name == record_name:
-            txt_record = record_set
-            break
-    if txt_record is not None:
-        print('removing old TXT record for letsencrypt challange')
-        change_info = txt_record.delete()
-        print(change_info)
-    print('creating TXT record for letsencrypt challange')
+    return route53_conn.get_hosted_zone_by_id(route53_id)
+
+def get_txt_record(domain):
+    """
+    Let's Encrypt expects the following TXT record:
+    _acme-challenge.{{ domain }}.
+    Note the ending period
+    """
+    return '_acme-challenge.{0}.'.format(domain)
+
+def upload_dns_challenge(domain, challenge):
+    """
+    Create TXT record for the specified domain in Route53
+    """
+    zone = get_route53_zone(domain)
+    record_name = get_txt_record(domain)
+    cleanup_dns_challenge(domain, challenge)
+    print('creating TXT record for letsencrypt challenge')
     # Route53 expects values to be "double-quoted"
-    le_challange = '"{}"'.format(le_challange)
+    le_challenge = '"{}"'.format(challenge)
     txt_record, change_info = zone.create_txt_record(
         # Notice that this is a full-qualified name.
         name=record_name,
-        # let's encrypt challange
+        # let's encrypt challenge
         # Route53 expects values to be "double-quoted"
-        values=[le_challange],
+        values=[le_challenge],
         ttl=TTL
     )
     # wait for DNS to propogate
@@ -74,40 +76,102 @@ def upload_dns_challange(domain, challenge):
     # verify DNS propogation
     answers = dns.resolver.query(record_name, 'TXT')
     for txtdata in answers:
-        if le_challange != str(txtdata):
+        if le_challenge != str(txtdata):
             raise ValueError('TXT {0} failed to propogate'.format(record_name))
 
-def cleanup_dns_challange(domain, challenge):
-    print('removing DNS challange')
+def cleanup_dns_challenge(domain, challenge):
+    """
+    Delete TXT record
+    """
+    print('removing DNS challenge')
+    zone = get_route53_zone(domain)
+    record_name = get_txt_record(domain)
+    # check for existing TXT challenge record
+    txt_record = None
+    for record_set in zone.record_sets:
+        if record_set.name == record_name:
+            txt_record = record_set
+            break
+    if txt_record is not None:
+        print('removing old TXT record for letsencrypt challenge')
+        change_info = txt_record.delete()
+        print(change_info)
+
 
 def deploy_to_api_gateway(domain, certfile):
     print('deploying certificates to API Gateway')
-    api_gateway_client = boto3.client('apigateway')
-    path, filename = os.path.split(certfile)
-    cert_body = None
-    with open(certfile) as cert:
-        cert_body = cert.read()
-    # get contents of the private key
-    private_key = None
-    with open(os.path.join(path, PRIVATE_KEY_FILE)) as cert:
-        private_key = cert.read()
-    # get contents of the certificate chain
-    chain = None
-    with open(os.path.join(path, CHAIN_FILE)) as cert:
-        chain = cert.read()
-    print(cert_body)
-    print()
-    print(private_key)
-    print()
-    print(chain)
-    response = api_gateway_client.create_domain_name(
-        domainName=domain,
-        certificateName=os.path.basename(certfile),
-        certificateBody=cert_body,
-        certificatePrivateKey=private_key,
-        certificateChain=chain
+    api_gateway_client = boto3.client(
+        'apigateway',
+        aws_access_key_id=os.environ['AWS_ACCESS_KEY'],
+        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
     )
-    pprint(response)
+    iam = boto3.resource('iam')
+    iam_client = boto3.client(
+        'iam',
+        aws_access_key_id=os.environ['AWS_ACCESS_KEY'],
+        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
+    )
+    path, filename = os.path.split(certfile)
+    certificate_body = None
+    with open(certfile) as cert:
+        certificate_body = cert.read()
+    # get contents of the private key
+    certificate_private_key = None
+    with open(os.path.join(path, PRIVATE_KEY_FILE)) as cert:
+        certificate_private_key = cert.read()
+    # get contents of the certificate certificate_chain
+    certificate_chain = None
+    with open(os.path.join(path, CHAIN_FILE)) as cert:
+        certificate_chain = cert.read()
+    api = None
+    try:
+        try:
+            api = api_gateway_client.get_domain_name(
+                domainName=domain
+            )
+        except botocore.exceptions.ClientError as e:
+            print(e.response['Error']['Code'])
+            api = None
+        if api is None:
+            print('creating domain name ({0}) in API Gateway'.format(domain))
+            api = api_gateway_client.create_domain_name(
+                domainName=domain,
+                certificateName=os.path.basename(certfile),
+                certificateBody=certificate_body,
+                certificatePrivateKey=certificate_private_key,
+                certificateChain=certificate_chain
+            )
+        else:
+            print('renewing domain name ({0}) in API Gateway'.format(domain))
+            # Zappa showed the way!
+            # Zappa/zappa/letsencrypt.py
+            # upload certificate to IAM
+            new_cert_name = domain + str(time.time())
+            iam.create_server_certificate(
+                ServerCertificateName=new_cert_name,
+                CertificateBody=certificate_body,
+                PrivateKey=certificate_private_key,
+                CertificateChain=certificate_chain
+            )
+            # point certificate to API Gateway
+            api_gateway_client.update_domain_name(
+                domainName=domain,
+                patchOperations=[
+                    {
+                        'op': 'replace',
+                        'path': '/certificateName',
+                        'value': new_cert_name,
+                    }
+                ]
+            )
+
+        # create CNAME record that points the 
+        cloud_front_distribution_name = api['distributionDomainName']
+        print('Create Alias record from {0} to {1}'.format(domain, cloud_front_distribution_name))
+    except botocore.exceptions.ClientError as e:
+        print('Failed to create API')
+        print(e.response['Error']['Code'])
+        raise(e)
 
 if __name__ == "__main__":
     hook = sys.argv[1]
@@ -119,8 +183,10 @@ if __name__ == "__main__":
     print(domain)
     print(txt_challenge)
     if hook == 'deploy_challenge':
-        upload_dns_challange(domain, txt_challenge)
+        upload_dns_challenge(domain, txt_challenge)
     if hook == 'clean_challenge':
-        cleanup_dns_challange(domain, txt_challenge)
+        cleanup_dns_challenge(domain, txt_challenge)
     if hook == 'deploy_cert':
+        deploy_to_api_gateway(domain, cert)
+    if hook == 'unchanged_cert':
         deploy_to_api_gateway(domain, cert)
